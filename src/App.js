c98a9574365b797ec313ff01bdaa20b1d9be4b2b
Adetth <raju.adetth@gmail.com>
2024-03-01 22:10:57 +0530

    Everybody's Circulation Fixed

--- src/App.js
+++ src/App.js
@@ -2,26 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let wid = Math.floor(window.innerWidth * (80/100));
+  let hgt = Math.floor(window.innerHeight * (80/100));
+
+  console.log(wid,hgt);
+
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <iframe width="420" height="315"
-        src="https://www.youtube.com/embed/tgbNymZ7vqY">
+        <iframe width={wid} height={hgt}
+        src="https://www.youtube.com/embed/RQmEERvqq70">
         </iframe> 
         <p>
           My goofy ahh website 😎😎
 
-          (ayushi is gay (this is proof that its my website))
+          {/* (ayushi is gay (this is proof that its my website)) */}
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
